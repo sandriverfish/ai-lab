5bedddf3e004f7bf5bb508af94a39129f515ba42
sandriverfish <mc6066@outlook.com>
2025-05-01 21:18:25 +0800

    0.0.15.12

--- wukong_v15/utils/image_processing.py
+++ wukong_v15/utils/image_processing.py
@@ -3,7 +3,12 @@ from PIL import Image
 class ImageSaver:
     @staticmethod
     def save_image(image, path, quality=95):
-        if isinstance(image, Image.Image):
+        if isinstance(image, list):
+            if len(image) > 0 and isinstance(image[0], Image.Image):
+                image[0].save(path, quality=quality)
+            else:
+                raise TypeError("List contains no valid images")
+        elif isinstance(image, Image.Image):
             image.save(path, quality=quality)
         else:
-            raise TypeError("Provided object is not an image")
+            raise TypeError("Provided object is not an image or list of images")
